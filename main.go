6a6b68315fe8322a18aa9c944887f092fe09624e
ywchiang0819121 <ywchiang0819121@gmail.com>
2019-07-02 23:14:05 +0800

    gg

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ func main() {
     cmd := "cat /var/log/iptables.log"
     out, err := exec.Command("bash", "-c", cmd).Output()
     if err == nil {
-        fmt.Println(string(out))
+		// fmt.Println(string(out))
+		var host string
+		var timestmp string
+		var etw string
+		while(fmt.Sscanf(string(out), "%s gamelab-MS-7A68 %s SRC=%s DST%s", &timestmp, &etw, &host, &etw)){
+			fmt.Println(timestmp, host)
+		}
     }
 }
